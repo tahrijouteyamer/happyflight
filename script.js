86e3c7ac12e726408a990ff15ecbf0c65aadbb5b
Amer Tahri-Joutey <125614736+tahrijouteyamer@users.noreply.github.com>
2024-10-11 02:51:14 +0100

    Update index.html and script.js

--- script.js
+++ script.js
@@ -13,3 +13,5 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
     header.classList.toggle("sticky", window.scrollY > 60);
 });
+
+// Rest of the code will be added later
