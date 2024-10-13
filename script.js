cc0f19483c43cec95f5aae8d21e70384ad7525cd
Amer Tahri-Joutey <125614736+tahrijouteyamer@users.noreply.github.com>
2024-10-13 16:18:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -32,6 +32,47 @@ navLinks.forEach(link => {
 }
 );
 
+const scrollLinks = document.querySelectorAll(".scroll-link");
 
+scrollLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navlist.classList.remove("open");
+        menu.classList.remove("bx-x");
+    });
+}
+);
 
-// Rest of the code will be added later
+const sections = document.querySelectorAll("section[id]");
+window.addEventListener("scroll", scrollActive);
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(".navlist a[href*=" + sectionId + "]").classList.add("active");
+        } else {
+            document.querySelector(".navlist a[href*=" + sectionId + "]").classList.remove("active");
+        }
+    });
+}
+
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "30px",
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.home-content, .home-img,
+            .about-content, .about-img,
+            .services-content, .services-img,
+            .contact-content, .contact-img,
+            .footer-content`, {
+    interval: 200
+});
+
